40f7f3dc5d5b1d6864fe3fd6ecfda9278d1a03ff
erfajc97 <erfacrypto@gmail.com>
2025-11-22 16:03:29 -0300

    [FIX] cors

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:5173',
       'http://localhost:5174',
+      'https://gestor-judicial.netlify.app',
       process.env.FRONTEND_URL || '*',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
